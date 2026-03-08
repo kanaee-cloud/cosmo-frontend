cf3a5037b19200e49f44b0d8ae672efc0ff09cc7
kanaee-cloud <arsalnaufal2007@gmail.com>
2026-03-08 22:29:53 +0700

    feat: setup configuration tailwind and font

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Lexend", "sans-serif"],
+        secondary: ["Silkscreen", "monospace"],
+        tertiary: ["VT323", "monospace"],
+        press: ['"Press Start 2P"', "cursive"]
+      },
+      colors: {
+        primary: "#0d0514",
+        secondary: "#1a0b2e",
+        tertiary: "#140724",
+        light: "#8a6dfc",
+        accent: "#ff0055"
+      }
+    },
+  },
+  plugins: [],
+}
+
