0138d75e91189f3cb084b8238a750025fdd2bcad
kanaee-cloud <arsalnaufal2007@gmail.com>
2026-03-14 11:56:39 +0700

    docs: vercel configuration

--- src/layouts/LandingLayout.jsx
+++ src/layouts/LandingLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom"
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar"
 import bgImage from '../assets/dashboard/bg-image.jpg'
 import Footer from '../components/Footer'
 
